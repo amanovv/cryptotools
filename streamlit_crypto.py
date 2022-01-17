45d1cf37f2da92c89a38c75ba99b78a11703b7bb
D <damanov@g.harvard.edu>
2022-01-17 07:47:52 -0500

    tweaks

--- streamlit_crypto.py
+++ streamlit_crypto.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     model = define_model()
     n_sentence = 3
     col1, col2 = st.columns(2)
-    number_of_news = col1.number_input("How many news you want?", min_value = 5, max_value = 20, value = 'int')
+    number_of_news = col1.number_input("How many news you want?", min_value = 5, max_value = 20)
     keyword_input = col2.text_input("Type the keyword here")
     if keyword_input:
         s = KeywordScraper(keyword_input)
